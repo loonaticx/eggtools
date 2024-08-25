87e41a43d7d4fecd9370102f51230a3110dd4cfa
Erica M. ("Loonatic") <l.oony@aol.com>
2024-08-25 00:21:41 -0500

    Examples: Simple EggDepalettizer script

---
+++ examples/utils/EggDepalettizer.py
@@ -0,0 +1,13 @@
+from . import *
+
+# test_egg defined in __init__
+
+from eggtools.utils.EggDepalettizer import Depalettizer
+
+# padding_u and padding_v should be between 0 and 1
+depal = Depalettizer([test_egg], padding_u = 0.06, padding_v = 0.06, eggman = eggman)
+
+depal.depalettize_all()
+
+for eggdata in depal.eggman.egg_datas.keys():
+    depal.eggman.write_egg_manually(eggdata, custom_suffix = "crop3.egg")
