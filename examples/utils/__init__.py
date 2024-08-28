3db3114ccae0306ba23b2ee2f5f248716d7bc56e
Erica M. ("Loonatic") <l.oony@aol.com>
2024-08-28 18:33:56 -0500

    Examples: Easier to manage model init setup

--- examples/utils/__init__.py
+++ examples/utils/__init__.py
@@ -3,15 +3,18 @@ from panda3d.core import Filename
 
 from eggtools.EggMan import EggMan
 
-if not os.path.isfile('tests/models/test_grid_1.egg'):
-    test_egg = Filename.fromOsSpecific('models/test_grid_1.egg')
+model_filename = "test_grid_1.egg"
+base_dir = "tests"
+
+if not os.path.isfile(f'{base_dir}/models/{model_filename}'):
+    test_egg = Filename.fromOsSpecific(f'models/{model_filename}')
 else:
-    test_egg = Filename.fromOsSpecific(os.path.join(os.getcwd(), 'tests/models/test_grid_1.egg'))
+    test_egg = Filename.fromOsSpecific(os.path.join(os.getcwd(), f'{base_dir}/models/{model_filename}'))
 
 eggman = EggMan(
     egg_filepaths = [test_egg],
     search_paths = [
-        os.path.join(os.getcwd(), "tests/maps"),
+        os.path.join(os.getcwd(), f"{base_dir}/maps"),
     ],
 )
 egg = eggman.get_egg_by_filename(test_egg)
