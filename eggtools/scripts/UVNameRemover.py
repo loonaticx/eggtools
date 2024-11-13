084ac44ed1ee0abda2ab38d17e5a5f83c81309c0
Erica M. ("Loonatic") <l.oony@aol.com>
2024-11-12 20:58:34 -0600

    UVNameRemover: Add wildcard support

--- eggtools/scripts/UVNameRemover.py
+++ eggtools/scripts/UVNameRemover.py
@@ -2,65 +2,78 @@
 Use this to check for models with non-empty UVMap names (known to cause issues)
 """
 
-from panda3d.core import Filename
 import os
 import sys
+import glob
+from panda3d.core import Filename
 from eggtools.EggMan import EggMan
-
-# argparse setup
+from eggtools.attributes.EggUVNameAttribute import EggUVNameAttribute
 import argparse
 
-from eggtools.attributes.EggUVNameAttribute import EggUVNameAttribute
-
+# region argparse setup
 parser = argparse.ArgumentParser(
-    prog='egg-uv-remover',
-    epilog='Removes UV names off a given egg file.',
-    description='python -m eggtools.scripts.UVNameRemover input.egg -o output.egg'
+    prog = 'egg-uv-remover',
+    epilog = 'Removes UV names off given egg file(s).',
+    description = 'python -m eggtools.scripts.UVNameRemover input.egg -o output.egg'
 )
 
 parser.add_argument(
     'input_egg',
+    help = 'Input egg file or wildcard pattern'
 )
 
 parser.add_argument(
     '-o',
     '--output',
-    # '-->',
-    type=str,
-    nargs='?',
-    help='Output egg filename.',
+    type = str,
+    nargs = '?',
+    help = 'Output egg filename. Only works if a single input file is provided.'
 )
+
 parser.add_argument(
     '--inplace',
-    default=False,
-    action='store_true',
-    help='If this option is given, the input egg files will be rewritten in place with the results.',
+    default = False,
+    action = 'store_true',
+    help = 'If this option is given, the input egg files will be rewritten in place with the results.'
 )
+
 args = parser.parse_args()
-input_loc = args.input_egg
-if not input_loc:
-    print("Error: You must specify an input egg file!")
+# endregion
+
+# Handle wildcard input
+input_files = sorted(glob.glob(args.input_egg))
+if not input_files:
+    print("Error: No files matched the input pattern!")
     sys.exit()
 
-input_egg = Filename.fromOsSpecific(input_loc)
-
-output_loc = args.output
-if not args.output and args.inplace:
-    output_loc = os.path.join(os.getcwd(), args.input_egg)
-if not output_loc:
-    print("Error: You must either specify an output filepath with -o or use --inplace.")
+if len(input_files) > 1 and args.output:
+    print("Error: Output filename can only be specified for a single input file.")
     sys.exit()
-output_file = Filename.fromOsSpecific(os.path.join(os.getcwd(), output_loc))
 
-eggman = EggMan([input_egg])
-eggdata = eggman.get_egg_by_filename(input_egg)
-ctx = eggman.egg_datas[eggdata]
-for eggattr in ctx.egg_attributes:
-    if isinstance(eggattr, EggUVNameAttribute):
-        print(f"Removing {eggattr} from {ctx.filename}")
-        eggattr.apply(eggdata, ctx)
-        ctx.dirty = True
-if not ctx.dirty:
-    print(f"Couldn't find a UVNameAttribute from {ctx.filename}, not doing anything.")
+# Process each file
+for input_loc in input_files:
+    input_egg = Filename.fromOsSpecific(input_loc)
 
-eggman.write_egg(eggdata, output_file)
+    # Determine output location
+    if args.inplace:
+        output_file = input_egg
+    else:
+        output_loc = args.output if args.output else os.path.splitext(input_loc)[0] + "-no_uvnames.egg"
+        output_file = Filename.fromOsSpecific(os.path.join(os.getcwd(), output_loc))
+
+    eggman = EggMan([input_egg])
+    eggdata = eggman.get_egg_by_filename(input_egg)
+    ctx = eggman.egg_datas[eggdata]
+
+    # Remove UV names
+    for eggattr in ctx.egg_attributes:
+        if isinstance(eggattr, EggUVNameAttribute):
+            print(f"Removing {eggattr} from {ctx.filename}")
+            eggattr.apply(eggdata, ctx)
+            ctx.dirty = True
+
+    if ctx.dirty:
+        eggman.write_egg(eggdata, output_file)
+        print(f"Processed {input_loc} -> {output_file}")
+    else:
+        print(f"Couldn't find a UVNameAttribute in {ctx.filename}, not doing anything.")
